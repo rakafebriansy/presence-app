870cc37f31c08b2588855e8db131fda49b7efc45
rakafebriansy <kerenr445@gmail.com>
2025-01-26 10:14:56 +0700

    feat: upload profile image

--- lib/app/modules/update_profile/controllers/update_profile_controller.dart
+++ lib/app/modules/update_profile/controllers/update_profile_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,6 +8,7 @@ import 'package:presence_app/app/errors/error_bags.dart';
 import 'package:presence_app/app/errors/validation_error.dart';
 import 'package:presence_app/app/helper/validators.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class UpdateProfileController extends GetxController with ErrorBags {
   RxBool isLoading = false.obs;
@@ -18,7 +21,7 @@ class UpdateProfileController extends GetxController with ErrorBags {
 
   void pickImage() async {
     final ImagePicker picker = ImagePicker();
-   this.pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    this.pickedImage = await picker.pickImage(source: ImageSource.gallery);
     update();
   }
 
@@ -47,18 +50,32 @@ class UpdateProfileController extends GetxController with ErrorBags {
       this.errorCheck();
 
       String uid = await FirebaseAuth.instance.currentUser!.uid;
-
-      await FirebaseFirestore.instance.collection('employees').doc(uid).update({
+      Map<String, String> data = <String, String>{
         'name': this.nameC.text,
-        'identification_number': this.identificationNumberC.text
-      });
+        'identification_number': this.identificationNumberC.text,
+      };
+
+      if (pickedImage != null) {
+        FirebaseStorage storage = FirebaseStorage.instance;
+        File file = File(this.pickedImage!.path);
+        String extension = this.pickedImage!.name.split('.').last;
+        await storage.ref('/$uid/profile.$extension').putFile(file);
+
+        String imageUrl =
+            await storage.ref('/$uid/profile.$extension').getDownloadURL();
+        data.addAll({'image': imageUrl});
+      }
+
+      await FirebaseFirestore.instance
+          .collection('employees')
+          .doc(uid)
+          .update(data);
 
       Get.back();
       Get.snackbar('Successfully update profile!', 'Your profile is updated.');
     } on FirebaseException catch (error) {
       print(error);
-      // TODO
-      throw error;
+      Get.snackbar('Failed to update profile!', error.message!);
     } on ValidationError catch (error) {
       print(error.toString());
       Get.snackbar('Failed to update profile!', error.getMessage());
